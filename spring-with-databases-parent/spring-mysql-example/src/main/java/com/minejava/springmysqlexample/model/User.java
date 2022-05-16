12f3ee1384e950bf1624134552a1bfb0f1a4ec21
MHipo <manes.hipocrates@gmail.com>
2022-05-16 21:37:03 +0000

    Fixed issues with spring security, mysql and spring jpa

--- spring-with-databases-parent/spring-mysql-example/src/main/java/com/minejava/springmysqlexample/model/User.java
+++ spring-with-databases-parent/spring-mysql-example/src/main/java/com/minejava/springmysqlexample/model/User.java
@@ -17,7 +17,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @NotBlank
     @Size(max = 20)
@@ -53,11 +53,11 @@ public class User {
         this.password = password;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
