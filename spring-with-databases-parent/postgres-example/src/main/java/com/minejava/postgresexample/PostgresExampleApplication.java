ebe101bba75d98c81c7c8280e13f1082325f855a
Diogenes4Sinope <talk2nategwu@protonmail.com>
2022-05-26 13:05:47 +0000

    Added data for the postgres repo

--- spring-with-databases-parent/postgres-example/src/main/java/com/minejava/postgresexample/PostgresExampleApplication.java
+++ spring-with-databases-parent/postgres-example/src/main/java/com/minejava/postgresexample/PostgresExampleApplication.java
@@ -10,6 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostgresExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(PostgresExampleApplication.class, args);
-    
+    }
 }
 
