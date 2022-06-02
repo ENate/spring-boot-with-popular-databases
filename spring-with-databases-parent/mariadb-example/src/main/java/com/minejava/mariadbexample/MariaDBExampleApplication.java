2c292853f12c05a94306238bc831e6a676e52398
negwu01 <negwu01@gmail.com>
2022-06-02 09:55:11 +0000

    Saved data in mariadb

--- spring-with-databases-parent/mariadb-example/src/main/java/com/minejava/mariadbexample/MariaDBExampleApplication.java
+++ spring-with-databases-parent/mariadb-example/src/main/java/com/minejava/mariadbexample/MariaDBExampleApplication.java
@@ -1,11 +1,30 @@
 package com.minejava.mariadbexample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.minejava.mariadbexample.repository.CustomerRepository;
+
 @SpringBootApplication
-public class MariaDBExampleApplication {
+public class MariaDBExampleApplication implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(MariaDBExampleApplication.class);
+    
+    @Autowired
+    private CustomerRepository customerRepository;
     public static void main(String[] args) {
         SpringApplication.run(MariaDBExampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // DONE: Auto-generated method stub
+        customerRepository.deleteAll();
+
+        // Save some data
+        
     }
 }
